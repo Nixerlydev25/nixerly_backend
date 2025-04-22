fef3fee0e9e8eb56ed665aa5f17c67c0e96414f6
naveedalirehmani <naveedalirehmani321@gmail.com>
2025-04-22 16:13:00 +0500

    Add Prisma schema files for notifications, OTPs, posts, reports, and users
    - Created new schema files for notifications, OTPs, posts, reports, and users to define their respective models and relationships.
    - Established relationships between users and other entities, including notifications, posts, and reports.
    - Included fields for managing user data, post details, and notification settings, laying the foundation for future features and interactions.

---
+++ src/routes/v1/global.router.ts
@@ -0,0 +1,34 @@
+import { Router } from "express";
+import authRouter from "./auth.router";
+import otpRouter from "./opt.router";
+import roleRouter from "./role.router";
+import restrictionsRouter from "./restrictions.router";
+import userRouter from "./user.router";
+import healthRouter from "./health.router";
+import subscriptionRouter from "./subscription.router";
+
+const Api2 = Router();
+// authentication. 
+//TODO : unit test.
+Api2.use("/auth", authRouter);
+
+// otp routes
+//TODO : unit test.
+Api2.use("/otp", otpRouter);
+
+// roles routes
+Api2.use("/roles", roleRouter);
+
+// Notification router for sending and saving notifications
+Api2.use("/restrictions", restrictionsRouter);
+
+Api2.use("/user", userRouter);
+
+// health route for server status
+//TODO : unit test.
+Api2.use("/health", healthRouter);
+
+// subscription routes
+Api2.use("/subscription", subscriptionRouter);
+
+export default Api2;
