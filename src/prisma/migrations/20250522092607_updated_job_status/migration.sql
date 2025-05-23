b643760519695bec015017ea0016c0868b6edd8a
naveedalirehmani <naveedalirehmani321@gmail.com>
2025-05-23 16:55:32 +0500

    migrations

--- src/prisma/migrations/20250522092607_updated_job_status/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - The values [IN_PROGRESS,COMPLETED,CANCELLED,EXPIRED] on the enum `jobs_status` will be removed. If these variants are still used in the database, this will fail.
-
-*/
--- AlterTable
-ALTER TABLE `jobs` MODIFY `status` ENUM('OPEN', 'CLOSED') NOT NULL DEFAULT 'OPEN';
